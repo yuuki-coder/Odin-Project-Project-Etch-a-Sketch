1c7646fb8ba0a59d73a1ba797d0c6553d9000dd3
Eidi Yuki <eidiyuki21@gmail.com>
2023-07-20 19:00:55 -0300

    UI adicionado.

---
+++ script.js
@@ -0,0 +1,8 @@
+const htmlColor = document.querySelector('.color')
+const htmlPixel = document.querySelector('.pixel')
+const htmlErase = document.querySelector('.erase')
+const htmlRainbow = document.querySelector('.rainbow')
+const htmlreset = document.querySelector('.reset')
+
+
+
